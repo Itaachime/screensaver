28b909db5c7d23c7a3272a560bc7094e2cc9178e
Dmitry Baranovskiy <dmitry@baranovskiy.com>
2013-02-09 10:42:19 +1100

    Added license information

--- ssaver.js
+++ ssaver.js
@@ -1,3 +1,17 @@
+// Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 window.onload = function () {
 	function Matrix(m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44) {
 		if (m11 == null) {
